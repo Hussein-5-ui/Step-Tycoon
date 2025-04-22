80fe3992d05eb5cc1953cff7e9bac842019d22ce
Jade <devlissiola@gmail.com>
2025-04-21 23:42:12 -0400

    Added statsview

--- app/src/main/java/com/example/steptycoon/StatsActivity.java
+++ app/src/main/java/com/example/steptycoon/StatsActivity.java
@@ -1,6 +1,8 @@
 package com.example.steptycoon;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,24 @@ public class StatsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //get the shared preferences
+        SharedPreferences preferences = getSharedPreferences("com.step_tycoon", MODE_PRIVATE);
+
+        //save them as variables
+        int steps = preferences.getInt("Steps", 0);
+        long cps = preferences.getLong("CPS", 0);
+        long balance = preferences.getLong("balance", 0);
+
+        //find the textviews
+        TextView tvStepsStat = findViewById(R.id.tvStepsStat);
+        TextView tvCPSStat = findViewById(R.id.tvCPSStat );
+        TextView tvBalanceStat = findViewById(R.id.tvBalanceStat);
+
+        //edit the textviews
+        tvStepsStat.setText("Steps: " + steps);
+        tvCPSStat.setText("CPS: " + cps);
+        tvBalanceStat.setText("Balance: " + balance);
+
     }
 }
